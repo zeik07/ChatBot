32fdff78a08af1b3e73e3c17a57492913e667a40
Kevin <klgvette07@gmail.com>
2018-06-04 17:33:19 -0400

    Commented code for easier navigation

--- ChatBot/ChatBot/Controllers/HomeController.cs
+++ ChatBot/ChatBot/Controllers/HomeController.cs
@@ -17,17 +17,22 @@ namespace ChatBot.Controllers
     {
         public async Task<IActionResult> Index()
         {
+            //Check to see if initial tokens have been recieved already to prevent double logins
             if (Models.Authenticate.InitialTokens != null)
             {
                 return Redirect("/Home/Dashboard");
             }
+            //Redirects to Twitch for Authorization of this app for use with the chosen account login
             Models.Authenticate.AuthorizationCode = HttpContext.Request.Query["code"];
+            //Sets the state of the Irc client to false for initial pass through, set to true on Irc client launch to prevent duplicate launches/messages in chat
             Models.Authenticate.IrcState = false;
+            //Checks to make sure that the Authorization Code is recieved from Twitch after the app has been authorized, then gets a set of authoization tokens
             if (Models.Authenticate.AuthorizationCode != null)
             {
                 AuthController Auth = new AuthController();
                 await Auth.GetTokens();
             }
+            //Checks to see that tokens were recieved and redirects to the dashboard if they were
             if (Models.Authenticate.ResponseBody != null)
             {                
                 return Redirect("/Home/Dashboard");
@@ -37,16 +42,22 @@ namespace ChatBot.Controllers
         
         public async Task<IActionResult> Dashboard()
         {
+            //Check to see if there is a Response Body with tokens to process
             if (Models.Authenticate.ResponseBody == null)
             {
                 return Redirect("/Home");
             }
             AuthController Auth = new AuthController();
+            //Sorts the initial tokens and gathers they one that are needed
             Auth.SortInitialTokens(JObject.Parse(Models.Authenticate.ResponseBody)).Children().ToList();
+            //Gets the Username and User ID from Twitch for the account that was used to Authenticate
             await Auth.GetUser(Models.Authenticate.InitialTokens["access_token"]);
             StreamInfoController Info = new StreamInfoController();
+            //Gets the Twitch channels title and game/catagory
             await Info.GetStreamInfoJson(Models.Authenticate.UserId);
+            //Gets the Twitch channels communities
             await Info.GetStreamCommunityJson(Models.Authenticate.UserId);
+            //Checks to see if the Irc client is started, if not then it starts the Irc client and sets the state to true to avoid duplicate launches
             if (Models.Authenticate.IrcState == false)
             {
                 IrcController Irc = new IrcController();
@@ -59,19 +70,23 @@ namespace ChatBot.Controllers
 
         public IActionResult Authenticate()
         {
+            //Redirects to the url used to login to Twitch to authenticate for the app
             string url = Models.Authenticate.LoginUrl;
             return Redirect(url);
         }
 
         public async Task<IActionResult> Update(StreamInfoViewModel streamInfoViewModel)
         {
+            //Retrieves the current set game, title, and communites from the dashboard webpage
             string game = streamInfoViewModel.Game;
             string title = streamInfoViewModel.Title;
             List<string> communities = streamInfoViewModel.Communities;
 
+            //Validates the login information to make sure the app is still authorized to makes changes
             AuthController Auth = new AuthController();
             await Auth.Validate();
 
+            //Updates the game, title, and communities on Twitch with the information on the dashboard
             UpdateController UpdateInfo = new UpdateController();
             //TODO: Only update title/game if changed, update all communities is any change
             await UpdateInfo.UpdateGame(game);
--- ChatBot/ChatBot/Controllers/IrcController.cs
+++ ChatBot/ChatBot/Controllers/IrcController.cs
@@ -22,6 +22,7 @@ namespace ChatBot.Controllers
 
         public void StartIrc()
         {            
+            //Starts the Irc client
             Irc.JoinRoom(UserName);            
             ChatThread = new Thread(GetMessage);
             ChatThread.Start();
@@ -29,6 +30,7 @@ namespace ChatBot.Controllers
         
         private void GetMessage()
         {
+            //Gets messages from the Irc client data stream
             ServerStream = Irc.tcpClient.GetStream();
             int buffSize = 0;
             byte[] inStream = new byte[10025];
@@ -49,19 +51,24 @@ namespace ChatBot.Controllers
 
         private void Msg()
         {
+            //Processes the message data from the data stream
             string[] separator = new string[] { "#zeik07 :" };
             string[] singlesep = new string[] { ":", "!" };
 
+            //Only processes actual chat messages and not Irc messages
             if (ReadData.Contains("PRIVMSG"))
             {
                 string username = ReadData.Split(singlesep, StringSplitOptions.None)[1];
                 string message = ReadData.Split(separator, StringSplitOptions.None)[1];
 
+                //Looks for messages that start with ! to process them for command calls
                 if (message[0] == '!')
                 {
                     Commands(username, message);
                 }
             }
+
+            //Processes Irc messages looking for "PING" in order to respond
             if (ReadData.Contains("PING"))
             {
                 Irc.PingResponse();
@@ -71,6 +78,7 @@ namespace ChatBot.Controllers
         private void Commands(string userName, string message)
         {
             string command = message.Split(new[] { ' ', '!' }, StringSplitOptions.None)[1];
+            //Checks to see if message containing the ! is actually a command and if so processes it for a response
             foreach (KeyValuePair<string, string> commands in CommandList.CommandsList)
             {
                 if (command.ToLower() == commands.Key.ToString().ToLower())
@@ -79,13 +87,13 @@ namespace ChatBot.Controllers
                     break;
                 }
             }
+
             /*switch (command.ToLower())
             {
                 case "test":
                     Irc.SendChatMessage("Testing!");
                     break;
             }*/
-
         }
     }
 
@@ -99,6 +107,7 @@ namespace ChatBot.Controllers
 
         public IrcClient(string ip, int port, string userName, string password)
         {
+            //Sends the required information to Twitch to connect to their Irc server
             tcpClient = new TcpClient(ip, port);
             inputStream = new StreamReader(tcpClient.GetStream());
             outputStream = new StreamWriter(tcpClient.GetStream());
@@ -113,6 +122,7 @@ namespace ChatBot.Controllers
 
         public void JoinRoom(string channel)
         {
+            //Joins the channel of the user that connect to the bot app
             this.channel = channel;
             outputStream.WriteLine("JOIN #" + channel);
             outputStream.Flush();
@@ -120,29 +130,34 @@ namespace ChatBot.Controllers
 
         public void LeaveRoom()
         {
+            //Leaves the channel the bot is in
             outputStream.Close();
             inputStream.Close();
         }
 
         public void SendIrcMessage(string message)
         {
+            //Sends an Irc message to the Twitch Irc server
             outputStream.WriteLine(message);
             outputStream.Flush();
         }
 
         public void SendChatMessage(string message)
         {
+            //Formats a message to send to the Twitch Irc server
             string userName = Authenticate.UserName.ToLower();
             SendIrcMessage(":" + userName + "!" + userName + "@" + userName + ".tmi.twitch.tv PRIVMSG #" + userName + " :" + message);
         }
 
         public void PingResponse()
         {
+            //Responds to the "PING" request from Twitch with "PONG"
             SendIrcMessage("PONG tmi.twitch.tv");
         }
 
         public string ReadMessage()
         {
+            // Clears the message variable and processes the current message
             string message = "";
             message = inputStream.ReadLine();
             return message;
